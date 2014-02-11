6e1fa167b191d87525845d2375d3d73e92313537
unknown <ACopeland@US-507-07.amsacs.org>
2014-02-11 14:28:47 -0500

    Here's my stuff ross

---
+++ Assets/LevelDesign/WinningBlockCode.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinningBlockCode : MonoBehaviour {
+	
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.name.Equals("CatParent")||other.gameObject.name.Equals("DogeParent")){
+			print("SAVIOR!");
+
+		}
+	}
+}
